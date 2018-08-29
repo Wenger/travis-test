b63233f43e3845078db50a6698e90aee9f02ef08
Robert Wenger <robwenger@gmail.com>
2018-08-29 17:17:59 -0400

    sudo

--- preinstall.sh
+++ preinstall.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-ls /usr/local/sbin
-sudo echo $PATH
-sudo cmake --vesion
-ls /usr/local/sbin/cmake/bin
-echo $PATH
-whoami
-cmake --version
+sudo grep 'cmake' -R /
+# ls /usr/local/sbin
+# sudo echo $PATH
+# sudo cmake --vesion
+# ls /usr/local/sbin/cmake/bin
+# echo $PATH
+# whoami
+# cmake --version
