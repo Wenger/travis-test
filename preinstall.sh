869439fe140ee2a31ee14e1964fc989902fc5848
Robert Wenger <robwenger@gmail.com>
2018-08-29 22:01:12 -0400

    fadksnfdsa

--- preinstall.sh
+++ preinstall.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sudo grep 'cmake' -R /
+# sudo grep 'cmake' -R /
 # ls /usr/local/sbin
 # sudo echo $PATH
 # sudo cmake --vesion
 # ls /usr/local/sbin/cmake/bin
 # echo $PATH
 # whoami
-# cmake --version
+cmake --version
